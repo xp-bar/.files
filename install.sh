55f24420343e7702f0ccc1f78307c0dcbda5063b
Nicholas Ireland <nick@nicholasireland.ca>
2018-12-18 08:48:52 -0500

    move symlinks to their own file

--- install.sh
+++ install.sh
@@ -124,22 +124,5 @@ pip install http-prompt
 # -- }}}
 
 # -- SYMLINKS -- {{{
-ln -sv $PWD/.global_gitignore ~/.gitignore
-ln -sv $PWD/.alias ~
-ln -sv $PWD/.bash_profile ~
-ln -sv $PWD/.env ~
-ln -sv $PWD/.function ~
-ln -sv $PWD/.gitignore_global ~
-ln -sv $PWD/.ignore ~
-ln -sv $PWD/.lynx/ ~
-ln -sv $PWD/.myclirc ~
-ln -sv $PWD/.newsboat ~
-ln -sv $PWD/.path ~
-rm ~/.tmux/tmux.conf
-ln -sv $PWD/.tmux.conf ~/.tmux/tmux.conf
-ln -sv $PWD/.nvim ~/.config/nvim/
-ln -sv $PWD/.vimrc ~
-ln -sv $PWD/.xvimrc ~
-ln -sv $PWD/.zsh-plugins ~
-ln -sv $PWD/.zshrc ~
+./sym.sh
 # -- }}}
