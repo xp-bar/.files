c3283cafb88979dc22fcf15951ddab7131954dc0
Nick Ireland <nick@nicholasireland.ca>
2019-05-11 00:08:12 -0400

    this was a whoops

--- sym.sh
+++ sym.sh
@@ -11,7 +11,6 @@ ln -sv $PWD/.nvimrc ~/.vimrc
 ln -sv $PWD/.xvimrc ~
 ln -sv $PWD/.zle ~
 ln -sv $PWD/z/z.sh ~
-ln -sv $PWD/z/zsh.sh ~
 ln -sv $PWD/.zsh-plugins ~
 ln -sv $PWD/.zshrc ~
 ln -sv $PWD/.koolaid ~
