f40c745db8d7c0c9264a959a6ba8c45c996dedbe
Nicholas Ireland <nick@nicholasireland.ca>
2021-02-24 13:53:01 -0500

    Update: change how nvm is installed

--- install.sh
+++ install.sh
@@ -103,11 +103,17 @@ brew install --cask firefox-developer-edition
 # -- }}}
 
 # -- npm installations -- {{{
-npm i -g nvm
-npm i -g avn avn-n avn-nvm
 npm i -g vue-cli
 npm install --global yarn
 # -- }}}
+
+# node version manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 10.15.1
 
 # git clone installations -- {{{
 DIR=$(pwd)
