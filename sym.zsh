b7a9da67c9fa7f1ec8281cea1deea1b955e8453d
Nicholas Ireland <nick@nicholasireland.ca>
2022-05-16 17:26:25 -0400

    Update: move .nvimrc -> nvim/init.vim

--- sym.zsh
+++ sym.zsh
@@ -23,7 +23,7 @@ touch ~/.env
 
 # NVIM stuff
 [ -d ~/.config/nvim/ ] || mkdir -p ~/.config/nvim/
-ln -sv $PWD/.nvimrc ~/.config/nvim/init.vim
+ln -sv $PWD/nvim/init.vim ~/.config/nvim/init.vim
 [ -d ~/.config/nvim/colors/ ] || mkdir -p ~/.config/nvim/colors/
 ln -sv $PWD/nvim/colors/two.vim ~/.config/nvim/colors
 [ -d ~/.config/nvim/autoload/lightline/colorscheme/ ] || mkdir -p ~/.config/nvim/autoload/lightline/colorscheme/
