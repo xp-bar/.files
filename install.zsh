e15b5087b0377b9a699ec6aff5e8fe855821dd9a
Nicholas Ireland <nick@nicholasireland.ca>
2023-07-28 11:51:52 -0400

    Update: swap from Vundle (dead, unmaintained) to vim-plug (more features, maintained)

--- install.zsh
+++ install.zsh
@@ -73,8 +73,9 @@ mkdir -p ~/.config
 mkdir -p ~/.config/nvim/
 # ---}}}
 
-# -- Install Vundle -- {{{
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+# -- Install vim-plug -- {{{
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # -- }}}
 
 # -- Install TPM -- {{{
