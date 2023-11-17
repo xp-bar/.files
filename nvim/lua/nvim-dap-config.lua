8bed901d547b39ac5222c4fd62fd948ee9c6199e
Nicholas Ireland <nick@nicholasireland.ca>
2023-11-17 14:45:16 -0500

    Add: slightly modified default config for nvim-dap-ui

--- nvim/lua/nvim-dap-config.lua
+++ nvim/lua/nvim-dap-config.lua
@@ -30,4 +30,84 @@ dap.configurations.php = {
 vim.keymap.set('n', '<leader>dk', function() dap.continue() end)
 vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end)
 
-require("dapui").setup()
+require("dapui").setup({
+  controls = {
+    element = "repl",
+    enabled = true,
+    icons = {
+      disconnect = "",
+      pause = "",
+      play = "",
+      run_last = "",
+      step_back = "",
+      step_into = "",
+      step_out = "",
+      step_over = "",
+      terminate = ""
+    }
+  },
+  element_mappings = {},
+  expand_lines = true,
+  floating = {
+    border = "single",
+    mappings = {
+      close = {"q", "<Esc>"}
+    }
+  },
+  force_buffers = true,
+  icons = {
+    collapsed = "",
+    current_frame = "ﰂ",
+    expanded = ""
+  },
+  layouts = {
+    {
+      elements = {
+        {
+          id = "scopes",
+          size = 0.25
+        },
+        {
+          id = "breakpoints",
+          size = 0.25
+        },
+        {
+          id = "stacks",
+          size = 0.25
+        },
+        {
+          id = "watches",
+          size = 0.25
+        }
+      },
+      position = "left",
+      size = 40
+    },
+    {
+      elements = {
+        {
+          id = "repl",
+          size = 0.5
+        },
+        {
+          id = "console",
+          size = 0.5
+        }
+      },
+      position = "bottom",
+      size = 10
+    }
+  },
+  mappings = {
+    edit = "e",
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    repl = "r",
+    toggle = "t"
+  },
+  render = {
+    indent = 1,
+    max_value_lines = 100
+  }
+})
