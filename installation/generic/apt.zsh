ad9c9164c90d3451da282151d324ebd37d2673c1
Nicholas Ireland <nick@nicholasireland.ca>
2024-01-24 15:06:02 -0500

    Add: install script for apt installs

---
+++ installation/generic/apt.zsh
@@ -0,0 +1,22 @@
+#! /bin/zsh
+
+sudo apt install ack
+sudo apt install rg
+sudo apt install antidote
+sudo apt install composer
+sudo apt install cowsay
+sudo apt install figlet
+sudo apt install fortune
+sudo apt install fzf
+sudo apt install jq
+sudo apt install mycli
+sudo apt install pgcli
+sudo apt install mysql
+sudo apt install postgres
+sudo apt install neovim
+sudo apt install nginx
+sudo apt install node
+sudo apt install php
+sudo apt install tmux
+sudo apt install toilet
+sudo apt install tree
