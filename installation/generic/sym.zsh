dc927a22ce4b2d82978d3d07e316526ddd82cbaa
Nicholas Ireland <nick@nicholasireland.ca>
2024-07-18 15:40:56 -0400

    Update: tmux remote config path and naming

--- installation/generic/sym.zsh
+++ installation/generic/sym.zsh
@@ -9,6 +9,7 @@ ln -sv $PWD/.zsh_plugins.txt ~
 ln -sv $PWD/.function ~
 ln -sv $PWD/.path ~
 ln -sv $PWD/.tmux.conf ~
+ln -sv $PWD/.tmux.remote.conf ~
 ln -sv $PWD/.zle ~
 ln -sv $PWD/z/z.sh ~
 ln -sv $PWD/.zshrc ~
