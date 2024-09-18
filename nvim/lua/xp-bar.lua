cd5f57878f747963eec7f594dd1d4aaaa07c05d4
Nicholas Ireland <nick@nicholasireland.ca>
2024-09-18 12:06:29 -0400

    Add: lua-ls config

--- nvim/lua/xp-bar.lua
+++ nvim/lua/xp-bar.lua
@@ -3,6 +3,30 @@
 
 require('nvim-dap-config')
 
+require'lspconfig'.lua_ls.setup{
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'Lua 5.3',
+        path = {
+          '?.lua',
+          '?/init.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.3/?.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.3/?/init.lua',
+          '/usr/share/5.3/?.lua',
+          '/usr/share/lua/5.3/?/init.lua'
+        }
+      },
+      workspace = {
+        library = {
+          vim.fn.expand'~/.luarocks/share/lua/5.3',
+          '/usr/share/lua/5.3',
+          '${3rd}/love2d/library'
+        }
+      }
+    }
+  }
+}
 require'lspconfig'.intelephense.setup{
   init_options = {
     licenceKey = os.getenv('INTELEPHENSE_LICENSE_KEY'),
