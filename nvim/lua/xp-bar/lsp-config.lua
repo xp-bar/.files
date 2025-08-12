007f896f4ecd4c6591b67c20a1f89727a462bab2
Nicholas Ireland <nick@nicholasireland.ca>
2025-08-12 15:10:13 -0400

    Update(lsp): include laravel-stubs directory (if exists) to load stubs for illuminate packages

--- nvim/lua/xp-bar/lsp-config.lua
+++ nvim/lua/xp-bar/lsp-config.lua
@@ -154,6 +154,16 @@ vim.lsp.config('intelephense', {
   capabilities = capabilities,
   init_options = {
     licenceKey = (os.getenv('INTELEPHENSE_LICENSE_KEY') or ''),
+  },
+  settings = {
+    intelephense = {
+      environment = {
+        includePaths = {
+          -- NOTE: this needs to be setup separately; is a separate Laravel install w/ stubs
+          os.getenv('HOME') .. '/Code/laravel-stubs'
+        }
+      }
+    }
   }
 })
 
