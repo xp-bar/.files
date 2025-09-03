3f9c7a5b655327fd55538be21a1977d0f916bab5
Nicholas Ireland <nick@nicholasireland.ca>
2025-09-03 13:23:18 -0400

    Add(nvim/lua): git helpers

--- nvim/lua/xp-bar/plugins/helpers/init.lua
+++ nvim/lua/xp-bar/plugins/helpers/init.lua
@@ -13,4 +13,18 @@ function M.map(list, func)
     return result
 end
 
+M.git = {}
+
+function M.git.branch()
+    return vim.fn.split(vim.fn.system('git rev-parse --abbrev-ref HEAD 2>/dev/null'), '\n', true)[1]
+end
+
+function M.git.diff()
+    return vim.fn.systemlist('git --no-pager diff --name-only 2>/dev/null')
+end
+
+function M.git.untracked()
+    return vim.fn.systemlist('git ls-files --others --exclude-standard 2>/dev/null')
+end
+
 return M
