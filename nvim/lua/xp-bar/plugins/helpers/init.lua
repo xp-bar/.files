486529dd459cb9514fa74f617b212b3a4e8aa74a
Nicholas Ireland <nick@nicholasireland.ca>
2025-09-03 13:23:10 -0400

    Add(nvim/lua): round / map helpers

--- nvim/lua/xp-bar/plugins/helpers/init.lua
+++ nvim/lua/xp-bar/plugins/helpers/init.lua
@@ -1,3 +1,16 @@
 local M = {}
 
+function M.round(number)
+    return math.floor(number+0.5)
+end
+
+function M.map(list, func)
+    local result = {}
+    for key, value in pairs(list) do
+        result[key] = func(value, key)
+    end
+
+    return result
+end
+
 return M
