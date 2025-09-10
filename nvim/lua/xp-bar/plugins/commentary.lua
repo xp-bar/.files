4a2d2e37698a270c2c93db1e606f92a86b949ddb
Nicholas Ireland <nick@nicholasireland.ca>
2025-09-10 16:15:54 -0400

    Update(nvim/commentary): add visual mode to mapping

--- nvim/lua/xp-bar/plugins/commentary.lua
+++ nvim/lua/xp-bar/plugins/commentary.lua
@@ -1,3 +1,3 @@
 -- https://stackoverflow.com/questions/9051837/how-to-map-c-to-toggle-comments-in-vim
 -- -- Vim recognizes C-_ as C-/, so control+/ will toggle comments
-vim.keymap.set('n', '<C-_>', ':Commentary<cr>')
+vim.keymap.set({'v', 'n'}, '<C-_>', ':Commentary<cr>')
