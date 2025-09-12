625bd89b506fef7392cdf4800f817c84b24dd732
Nicholas Ireland <nick@nicholasireland.ca>
2025-09-12 16:55:57 -0400

    Update(nvim/comment-nvim): remove default mappings since they overlap with builtins and my current gb = % mapping

--- nvim/lua/xp-bar/plugins/configs/comment-nvim.lua
+++ nvim/lua/xp-bar/plugins/configs/comment-nvim.lua
@@ -2,13 +2,17 @@ require('Comment').setup({
     padding = true,
     sticky = true,
     ignore = nil,
-    toggler = { line = '<C-_>', block = 'gbc' },
+    -- toggler = { line = '<C-_>', block = 'gbc' },
     opleader = { line = 'gc', block = 'gb' },
     extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
     mappings = {
-        basic = true, -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        -- we don't technically need `basic` since they're builtin
+        basic = false, -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        -- but the other motions are nice
         extra = true  -- Extra mapping; `gco`, `gcO`, `gcA`
     },
     pre_hook = nil,
     post_hook = nil,
 })
+
+vim.keymap.set({'n', 'v'}, '<C-_>', ':normal gcc<cr>', {silent = true})
