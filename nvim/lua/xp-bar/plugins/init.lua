dbd9d5a28c61ec64737ff5b2d5ca5bdbd98091ab
Nicholas Ireland <nick@nicholasireland.ca>
2025-09-16 10:08:41 -0400

    Remove(nvim/plugins): vim-surround

--- nvim/lua/xp-bar/plugins/init.lua
+++ nvim/lua/xp-bar/plugins/init.lua
@@ -34,7 +34,6 @@ Plugins.install('dhruvasagar/vim-zoom') -- tmux-style pane zoom
 Plugins.install('Raimondi/delimitMate') -- delimiter assistance
 Plugins.install('tpope/vim-fugitive') -- git
 Plugins.install('junegunn/gv.vim') -- git log browser
-Plugins.install('tpope/vim-surround') -- surround a text object w/ chars
 Plugins.install('ggandor/leap.nvim') -- extra movement motions
 Plugins.install('airblade/vim-rooter') -- find project root
 Plugins.install('mattn/emmet-vim', {['for'] = {'html', 'vue', 'eruby', 'blade.php', 'typescriptreact'}})
