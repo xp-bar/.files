56f70b1f869bb97d9675f7d06a5f7a05aecfc3cd
Nicholas Ireland <nick.ireland@roofr.com>
2025-11-06 10:17:32 -0500

    Add(nvim/modules): colors module

---
+++ nvim/lua/xp-bar/modules/colors.lua
@@ -0,0 +1,24 @@
+local M = {};
+
+M.colors = {
+    gunmetal      = '#282c34',
+    charcoal_1    = "#3E4451",
+    charcoal_2    = '#4b5263',
+    paynes_grey_1 = '#5c6370',
+    paynes_grey_2 = '#636d83',
+    slate_grey_1  = '#828997',
+    slate_grey_2  = '#7f848e',
+    french_grey   = '#abb2bf',
+    cyan          = '#56b6c2',
+    blue          = '#61afef',
+    purple        = '#c678dd',
+    green         = '#98c379',
+    red           = '#e06c75',
+    dark_red      = '#be5046',
+    orange        = '#d19a66',
+    yellow        = '#e5c07b',
+    pale_yellow   = '#e2be7d',
+};
+
+
+return M;
