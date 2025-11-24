0ba16ff36cc0f60a7f0f1413cb7d364d92f91566
Nicholas Ireland <nick.ireland@roofr.com>
2025-11-24 11:51:01 -0500

    Add(nvim): Calc usercommand

--- nvim/lua/xp-bar/packs.lua
+++ nvim/lua/xp-bar/packs.lua
@@ -13,3 +13,7 @@ for index, pack in ipairs(packs) do
     -- vim.pack.add() is only available in nightly currently
     vim.cmd('packadd! ' .. pack)
 end
+
+vim.api.nvim_create_user_command('Calc', function (args)
+    print('Result' .. vim.fn.system('bc <<< ' .. vim.fn.shellescape(args.args, true)))
+end, {nargs = '*'})
