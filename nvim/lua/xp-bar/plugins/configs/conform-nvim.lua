0d1ef6907ba79c7722215a9da3cb1eec447aec50
Nicholas Ireland <nick.ireland@roofr.com>
2025-12-15 12:38:23 -0500

    Add(nvim/plugins/conform): add php-cs-fixer formatter for php

--- nvim/lua/xp-bar/plugins/configs/conform-nvim.lua
+++ nvim/lua/xp-bar/plugins/configs/conform-nvim.lua
@@ -4,11 +4,26 @@ require('conform').setup({
         javascript = {'prettier', stop_after_first = true},
         typescript = {'prettier', stop_after_first = true},
         typescriptreact = {'prettier', stop_after_first = true},
+        php = {'php_cs_fixer', stop_after_first = true}
     },
+    formatters = {
+        php_cs_fixer = function (bufnr)
+            local config_file = os.getenv('PHP_CS_CONFIG_FILE')
+            if config_file then
+                return {append_args = {'--config', config_file}}
+            end
+
+            return {}
+        end
+    }
 });
 
 aug('prettierfix', function (au)
     au({'BufWritePre'}, '*.tsx,*.ts,*.mjs,*.js', function (args)
+        require("conform").format({ bufnr = args.buf })
+    end)
+
+    au({'BufWritePre'}, '*.php,*.blade.php', function (args)
         require("conform").format({ bufnr = args.buf })
     end)
 end)
