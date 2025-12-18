d0e542c522e51ca863bc5d50a8c3b4513d30e668
Nicholas Ireland <nick.ireland@roofr.com>
2025-12-18 16:25:59 -0500

    Update(nvim/plugins/alpha): quotes on quote

--- nvim/lua/xp-bar/plugins/configs/alpha-nvim/init.lua
+++ nvim/lua/xp-bar/plugins/configs/alpha-nvim/init.lua
@@ -45,9 +45,9 @@ require('alpha').setup({
         hr('_.-~-._', 45),
         padding(2),
         text({
-            'In the beginning the Universe was created.',
-            'This has made a lot of people very angry',
-            'and been widely regarded as a bad move.',
+            '"In the beginning the Universe was created.',
+            ' This has made a lot of people very angry',
+            ' and been widely regarded as a bad move."',
             '',
             '                         -- Douglas Adams'
         }, {hl = {
