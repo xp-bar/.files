a26bbe0ca56fe4533d616a1186ebec3906a6c163
Nicholas Ireland <nick.ireland@roofr.com>
2025-12-18 11:53:28 -0500

    Update(nvim/plugins): continue tweaking starting theme; still need some shortcuts but we're getting there

--- nvim/lua/xp-bar/plugins/configs/alpha-nvim/helpers/init.lua
+++ nvim/lua/xp-bar/plugins/configs/alpha-nvim/helpers/init.lua
@@ -18,12 +18,47 @@ M.group = function (fields, opts)
     }
 end
 
-M.field = function (value, opts, type)
+M.text = function (value, opts)
     return {
-        type = type or 'text',
+        type = 'text',
         val = value,
         opts = table_with_default(opts, {position = 'center'})
     }
+end
+
+-- adapted from require('alpha.themes.startify').button
+M.button = function (sc, txt, keybind, keybind_opts)
+    local sc_ = sc:gsub("%s", ""):gsub('SPC', "<leader>")
+
+    local opts = {
+        position = 'center',
+        shortcut = '[' .. sc .. '] ',
+        cursor = 1,
+        width = 50,
+        align_shortcut = 'left',
+        hl_shortcut = { { 'Operator', 0, 1 }, { 'Number', 1, #sc + 1 }, { 'Operator', #sc + 1, #sc + 2 } },
+        shrink_margin = false,
+    }
+    if keybind then
+        keybind_opts = table_with_default(keybind_opts, { noremap = true, silent = true, nowait = true })
+        opts.keymap = { 'n', sc_, keybind, keybind_opts }
+    end
+
+    local function on_press()
+        local key = vim.api.nvim_replace_termcodes(keybind .. '<Ignore>', true, false, true)
+        vim.api.nvim_feedkeys(key, 't', false)
+    end
+
+    return {
+        type = 'button',
+        val = txt,
+        on_press = on_press,
+        opts = opts,
+    }
+end
+
+M.edit_file_button = function(sc, filepath)
+    return M.button(sc, filepath, ':e '.. filepath .. '<CR>')
 end
 
 M.padding = function (height)
