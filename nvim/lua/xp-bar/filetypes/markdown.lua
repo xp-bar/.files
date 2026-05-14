b61c0d02d8907d42046235dfc13fddb2f5454b63
Nicholas Ireland <nick@nicholasireland.ca>
2026-05-13 22:25:31 -0400

    Update(nvim/plugins/nvim-cmp): markdown-specific rules for completion and extra keybind for triggering completion manually

--- nvim/lua/xp-bar/filetypes/markdown.lua
+++ nvim/lua/xp-bar/filetypes/markdown.lua
@@ -5,6 +5,16 @@ aug('filetypes.md', function (au)
     au({'BufNewFile', 'BufRead'}, '*.md', function()
         vim.bo.filetype = 'markdown'
         vim.opt_local.spell = true
+
+        local cmp = require('cmp')
+        cmp.setup.buffer({
+            mapping = cmp.mapping.preset.insert({
+                ['<S-Tab>'] = cmp.mapping.complete()
+            }),
+            completion = {
+                keyword_length = 4
+            },
+        })
     end)
 end)
 
