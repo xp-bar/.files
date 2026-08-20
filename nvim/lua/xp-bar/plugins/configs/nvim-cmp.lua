5a84be08edc985fc771f2dde1d3316b98c3f21c7
Nicholas Ireland <nick@nicholasireland.ca>
2026-08-20 09:39:35 -0400

    Update(nvim/plugins/nvim-cmp): adjust mappings

--- nvim/lua/xp-bar/plugins/configs/nvim-cmp.lua
+++ nvim/lua/xp-bar/plugins/configs/nvim-cmp.lua
@@ -9,14 +9,14 @@ cmp.setup {
   },
   mapping = cmp.mapping.preset.insert({
     ['<CR>'] = cmp.mapping.confirm {select = true},
-    ['<S-J>'] = cmp.mapping(function(fallback)
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       else
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-K>'] = cmp.mapping(function(fallback)
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       else
